613b7212a8a5d80a36a2effb1d48c81fd789d57e
Jorge <jpeg104@gmail.com>
2025-10-20 20:49:15 -0500

    update 2

--- test.java
+++ test.java
@@ -1,5 +1,14 @@
 public class test {
+
+    // Método Hola Mundo
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
     }
+
+    // Método Hello World
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+    //test
 }
